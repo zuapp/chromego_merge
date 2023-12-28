210f85c50e9e2a3d44567341cffe27bf5407530c
Mareep <62206240+vveg26@users.noreply.github.com>
2023-12-29 00:43:09 +0800

    Update merge.py

--- merge.py
+++ merge.py
@@ -111,6 +111,7 @@ def process_clash(data, index):
                     udp_relay_mode = proxy.get("udp-relay-mode", "naive")
                     congestion = proxy.get("congestion-controller", "bbr")
                     alpn = proxy.get("alpn", [])[0] if proxy.get("alpn") and len(proxy["alpn"]) > 0 else None
+                    location = get_physical_location(server)
                     name = f"{location}_tuic_{index}"
                     #tuic_meta_neko = f"tuic://{server}:{port}?uuid={uuid}&version=5&password={password}&insecure={insecure}&alpn={alpn}&mode={udp_relay_mode}"
                     tuic_meta = f"tuic://{uuid}:{password}@{server}:{port}?sni={sni}&congestion_control={congestion}&udp_relay_mode={udp_relay_mode}&alpn={alpn}&allow_insecure={insecure}#{name}"
@@ -124,6 +125,7 @@ def process_clash(data, index):
                     obfs_password = proxy.get("obfs-password","")
                     sni = proxy.get("sni", "")
                     insecure = int(proxy.get("skip-cert-verify", 0))
+                    location = get_physical_location(server)
                     name = f"{location}_hy2_{index}"
                     hy2_meta = f"hysteria2://{auth}@{server}:{port}?insecure={insecure}&sni={sni}&obfs={obfs}&obfs-password={obfs_password}#{name}"
                     merged_proxies.append(hy2_meta)
@@ -142,6 +144,7 @@ def process_clash(data, index):
                     fast_open = int(proxy.get("fast_open", 1))
                     auth = proxy.get("auth-str", "")
                     # 生成URL
+                    location = get_physical_location(server)
                     name = f"{location}_hy_{index}"
                     hysteria_meta = f"hysteria://{server}:{port}?peer={sni}&auth={auth}&insecure={insecure}&upmbps={up_mbps}&downmbps={down_mbps}&alpn={alpn}&mport={ports}&obfs={obfs}&protocol={protocol}&fastopen={fast_open}#{name}"
                     merged_proxies.append(hysteria_meta)
@@ -225,6 +228,7 @@ def process_hysteria(data, index):
         fast_open = int(json_data.get("fast_open", 0))
         auth = json_data.get("auth_str", "")
         # 生成URL
+        location = get_physical_location(server)
         name = f"{location}_hysteria_{index}"
         hysteria = f"hysteria://{server}?peer={server_name}&auth={auth}&insecure={insecure}&upmbps={up_mbps}&downmbps={down_mbps}&alpn={alpn}&obfs={obfs}&protocol={protocol}&fastopen={fast_open}#{name}"
         merged_proxies.append(hysteria)
@@ -243,6 +247,7 @@ def process_hysteria2(data, index):
         sni = json_data["tls"]["sni"]
         auth = json_data["auth"]
         # 生成URL
+        location = get_physical_location(server)
         name = f"{location}_hysteria2_{index}"
         hysteria2 = f"hysteria2://{auth}@{server}?insecure={insecure}&sni={sni}#{name}"
 
@@ -293,6 +298,7 @@ def process_xray(data, index):
             ws_settings = stream_settings.get("wsSettings", {})
             ws_path = ws_settings.get("path", "")
             ws_headers_host = ws_settings.get("headers", {}).get("Host", "")
+            location = get_physical_location(server)
             name = f"{location}_vless_{index}"
             xray_proxy = f"vless://{uuid}@{server}:{port}?security={security}&allowInsecure={insecure}&flow={flow}&type={network}&fp={fp}&pbk={publicKey}&sid={short_id}&sni={sni}&serviceName={grpc_serviceName}&path={ws_path}&host={ws_headers_host}#{name}"
 
