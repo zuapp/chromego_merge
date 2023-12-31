5bc19aa57fac13e6d17b2c2535f525c6720ab2bb
Mareep <62206240+vveg26@users.noreply.github.com>
2024-01-01 01:30:52 +0800

    Update meta_merge.py

--- meta_merge.py
+++ meta_merge.py
@@ -42,7 +42,7 @@ def get_physical_location(address):
         country = response.country.name
         city = response.city.name
         #return f"{country}_{city}"
-        return f"{country}"
+        return f"油管绵阿羊_{country}"
     except geoip2.errors.AddressNotFoundError as e:
         print(f"Error: {e}")
         return "Unknown"
