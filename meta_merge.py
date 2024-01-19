7f4fbad1899179ac0a273dcd386a8dcb70c0c2d2
Mareep <62206240+vveg26@users.noreply.github.com>
2024-01-19 12:25:59 +0800

    更新 meta_merge.py

--- meta_merge.py
+++ meta_merge.py
@@ -268,7 +268,7 @@ merged_proxies = []
 process_urls('./urls/clash_urls.txt', process_clash)
 
 # 处理 shadowtls URLs
-process_urls('./urls/sb_urls.txt', process_sb)
+#process_urls('./urls/sb_urls.txt', process_sb)
 
 # 处理 hysteria URLs
 process_urls('./urls/hysteria_urls.txt', process_hysteria)
